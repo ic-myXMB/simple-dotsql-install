27bb9d380f12fd0ae69082f56cb69e002c8e098a
ic-myXMB <106540361+ic-myXMB@users.noreply.github.com>
2022-06-25 06:17:59 -0700

    Update db-install.php

--- Upload/db-install.php
+++ Upload/db-install.php
@@ -173,6 +173,8 @@ function dbs_installed() {
 		echo "		<!DOCTYPE html>
 		<html lang=\"en\">
 		<meta charset=\"UTF-8\">
+		<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+		<head>
 		<title>Install Databse</title>
 		</head>
 		<body>
